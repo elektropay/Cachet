c45b0495a587ee85ef4e56e958ec6a2aa913d21f
Elektropay <richard.r@everpayinc.com>
2019-06-11 07:37:40 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -60,7 +60,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://status.elektropay.com'),
 
     /*
     |--------------------------------------------------------------------------
